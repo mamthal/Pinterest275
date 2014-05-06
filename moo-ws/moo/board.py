8e07d305666b4efbd4b82ac8994044defd1ba262
Mamtha Lakshminarasimhan <mamtha@mamtha-Dell.(none)>
2014-05-06 01:14:46 -0700

    added routes

---
+++ moo-ws/moo/board.py
@@ -0,0 +1,5 @@
+class Board:
+    def __init__(self, board_id, board_name):
+        self.board_id = board_id
+        self.board_name = board_name
+        
---
+++ moo-ws/moo/pin.py
@@ -0,0 +1,7 @@
+class Pin:
+    def __init__(self, pin_id, pin_name, pin_url):
+        self.pin_id = pin_id
+        self.pin_name = pin_name
+        self.pin_url = pin_url
+
+    
